e0c8cf8c09f36bb9cc9e73db316a32f19292ec65
Maarten van Stam <maarten@vanstam.org>
2022-01-06 22:56:22 +0100

    Created an Add-in from the Blazor Website ... things still fail, but we do have some interaction with Word here (local, manifest not yet updated to pick the Web App)

---
+++ BlazorAddIn/Pages/Counter.razor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+using System.Threading.Tasks;
+
+namespace BlazorAddIn.Pages
+{
+    public partial class Index
+    {
+        [Inject]
+        public IJSRuntime JSRuntime { get; set; }
+
+        private IJSObjectReference _jsModule;
+
+        protected override async Task OnInitializedAsync()
+        {
+            _jsModule = await JSRuntime.InvokeAsync<IJSObjectReference>("import", "./scripts/jsExamples.js");
+        }
+
+        private async Task ShowAlertWindow() =>
+            await _jsModule.InvokeVoidAsync("showAlert", new { Name = "John", Age = 35 });
+
+        private async Task InsertParagraph() =>
+            await _jsModule.InvokeVoidAsync("insertParagraph");
+    }
+}
---
+++ BlazorAddIn/wwwroot/scripts/jsExamples.js
@@ -0,0 +1,18 @@
+﻿export function showAlert(obj) {
+    const message = 'Name is ' + obj.name + ' Age is ' + obj.age;
+    alert(message);
+}
+
+export function insertParagraph() {
+
+    console.log("Hello JavaScript in Blazor!?!?!?");
+
+    return Word.run((context) => {
+
+        // insert a paragraph at the start of the document.
+        const paragraph = context.document.body.insertParagraph("Hello World", Word.InsertLocation.start);
+
+        // sync the context to run the previous API call, and return.
+        return context.sync();
+    });
+}
